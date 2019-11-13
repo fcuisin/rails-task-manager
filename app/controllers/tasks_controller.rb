9feee414a8d96040015aa5e064000f239da7e1d8
Ferdinand Cuisin <fcuisin@gmail.com>
2019-11-13 08:40:44 -0500

    Refactor methods with before_action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -13,28 +13,29 @@ class TasksController < ApplicationController
   end
 
   def create
-    task = Task.new(params_task)
-    task.save
-    redirect_to tasks_path(task)
+    @task = Task.new(params_task)
+    @task.save
+    redirect_to tasks_path(@task)
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(params_task)
     redirect_to tasks_path(@tasks)
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     redirect_to tasks_path
   end
 
   private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
 
   def params_task
     params.require(:task).permit(:title, :details, :completed)
